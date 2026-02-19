4e450b8438611a8ac2028dcd6c2856c4d96e999a
SikdarRudra <onsrudrasikdar@gmail.com>
2026-02-19 21:56:55 +0530

    Remove all files

--- routes/website.route.js
+++
@@ -1,8 +0,0 @@
-import {Router} from 'express';
-import { createContact } from '../controllers/contacts.controller.js';
-const websiteRoute = Router()
-
-
-websiteRoute.post('/contact',createContact)
-
-export default websiteRoute
